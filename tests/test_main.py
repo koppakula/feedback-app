6451309a5d76d6dc13fe309c4018fdfef472e302
EC2 Default User <ec2-user@ip-172-31-45-118.eu-north-1.compute.internal>
2025-07-07 09:26:47 +0000

    Test :home page renders form correctly

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,8 @@
-def test_sample():
-    assert 1 + 1 == 2
+from app import app
+
+def test_homepage_loads():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"<form" in response.data  # checks that your form is present
+
